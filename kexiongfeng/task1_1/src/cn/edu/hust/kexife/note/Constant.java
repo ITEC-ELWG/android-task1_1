72f78c2572f25234204b307d132f4a20c357a58b
kexife <kexife@gmail.com>
2014-07-11 23:56:36 +0800

    first commit

---
+++ kexiongfeng/task1_1/src/cn/edu/hust/kexife/note/Constant.java
@@ -0,0 +1,11 @@
+package cn.edu.hust.kexife.note;
+
+public class Constant {
+    public static final String TABLE_NAME = "notes";
+    public static final String COLUMN_NAME_ID = "_id";
+    public static final String COLUMN_NAME_NOTE = "note";
+    public static final String COLUMN_NAME_COLOR = "color";
+    public static final String COLUMN_NAME_CREATE_TIME = "created";
+    public static final String COLUMN_NAME_MODIFY_TIME = "modified";
+    public static enum  EDITE_MODE{ ADD, EDITE , DELETE }
+}
